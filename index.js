162cf4f176fd21f3471eae127885ae7835c97892
ilh0 <ljubijankicilhan@gmail.com>
2024-04-05 21:34:00 +0200

    home page progress

---
+++ index.js
@@ -0,0 +1,15 @@
+const hamburger = document.getElementById('hamburger');
+const navbarNav = document.querySelector('.navbarMenu ul');
+
+hamburger.addEventListener('click', function () {
+    navbarNav.classList.toggle('show');
+});
+
+const aktivnaStranica = window.location.pathname;
+
+const navLinkovi = document.querySelectorAll('nav a').forEach(link => {
+    if(link.href.includes(`${aktivnaStranica}`)){
+        link.classList.add('active');
+    }
+})
+
